77b3f01f17f3dbca754ef653215a94f7154c8bbf
Alina Kraieva <akraewaia@gmail.com>
2021-12-26 14:12:50 +0200

    first commit

---
+++ closures(du).playground/Contents.swift
@@ -0,0 +1,31 @@
+//uloha1
+//Provide the sorting closure.
+let numbs = [10, 3, 41, 72, 2, 11]
+var sortedNumbs = numbs.sorted(by: { (s1: Int, s2: Int) -> Bool in return s1 < s2})
+print(sortedNumbs)
+
+//or
+let numbs1 = [10, 3, 41, 72, 2, 11]
+var sortedNumbs1 = numbs1.sorted(by: { s1, s2 in return s1 < s2})
+print(sortedNumbs)
+
+//or
+let numbs2 = [10, 3, 41, 72, 2, 11]
+var sortedNumbs2 = numbs2.sorted(by: { s1, s2 in s1 < s2})
+print(sortedNumbs)
+
+//or
+let numbs3 = [10, 3, 41, 72, 2, 11]
+var sortedNumbs3 = numbs3.sorted(by: <)
+print(sortedNumbs)
+
+
+//uloha2
+//Defined a closure that returns the square of a number.
+
+var numbSquare = { (numb: Int) -> (Int) in
+  let square = numb * numb
+  return square
+}
+var findResult = numbSquare(12)
+print("result:", findResult)
